8742dded65bb70bbbbba57f0127859270f2ede52
Marco <marco.graffi15@gmail.com>
2019-10-01 18:43:05 -0300

    Buttons hidden

---
+++ WYHBM/Assets/Scripts/ScreenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+    public GameObject Exit;
+    public GameObject MenuActions;
+    public bool isPanelActive;
+
+
+  void Update()
+  {
+      if(isPanelActive)
+      {
+          Exit.SetActive(false);
+          MenuActions.SetActive(false);
+      }
+      else
+      {
+          Exit.SetActive(true);
+          MenuActions.SetActive(true);
+      }
+
+  }
+  
+}
