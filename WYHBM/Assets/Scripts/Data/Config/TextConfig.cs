362ee0daf2b499dbf22d3683267affb86889aaa9
Mariano <infernuzigt@gmail.com>
2019-10-24 08:11:41 -0300

    Win and Lose states

--- WYHBM/Assets/Scripts/Data/Config/TextConfig.cs
+++ WYHBM/Assets/Scripts/Data/Config/TextConfig.cs
@@ -8,6 +8,8 @@ public class TextConfig : ScriptableObject
     public string fadeOutText = "FINISH";
 
     [Header("Combat - UI")]
+    public string gameWinTxt = "WIN";
+    public string gameLoseTxt = "LOSE";
     public string playActionTxt = "EXECUTE";
     public string informationOneText = "{0}: <size=175>{1}</size>";
     public string informationTwoText = "{0}: <size=175>{1}-{2}</size>";
