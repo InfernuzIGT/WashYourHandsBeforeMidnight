7ad14c95a8ec1d197d1e7c0dde94eaac80056a18
Marco <marco.graffi15@gmail.com>
2020-03-04 21:41:38 -0300

    [WIP] Combat Refactor

---
+++ WYHBM/Assets/Scripts/CharacterCombatController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCombatController : MonoBehaviour
+{
+    private StatsSO stats;
+    private int _speedBase = 2;
+    private int _damageBase = 2;
+    private int _healthBase = 100;
+    private int _dexterityBase = 2;
+    private int _currentHealth;
+
+private void Start()
+{
+    Stats();
+}
+
+private void Update()
+{
+    if(Input.GetKeyDown(KeyCode.Space))
+    {
+        Attack();
+    }
+}
+
+private void Attack()
+{
+    _currentHealth = _healthBase;
+
+    
+    
+    
+}
+
+private void Stats()
+{
+    _speedBase *= stats.speed;
+    _damageBase *= stats.strength;
+    _healthBase *= stats.vitality;
+    _dexterityBase *= stats.dexterity;
+}
+}
---
+++ WYHBM/Assets/Scripts/StatsSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New StatsSO", menuName = "Scriptable Objects/Stats", order = 0)]
+public class StatsSO : ScriptableObject
+{   
+    [Header("Stats - General")]
+    
+    public int speed;
+    public int strength;
+    public int vitality;
+    public int dexterity;
+   
+}
