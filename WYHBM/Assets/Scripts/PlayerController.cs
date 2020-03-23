feb62ae3f54b115a5128b027ce11f98814f140ac
Mariano <infernuzigt@gmail.com>
2020-03-23 15:44:15 -0300

    Animations - Sam

--- WYHBM/Assets/Scripts/PlayerController.cs
+++ WYHBM/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 15f;
 
-    //private AnimatorController _animatorController;
+    private AnimatorController _animatorController;
 
     // Movement Values
     private float _moveHorizontal;
@@ -16,39 +16,39 @@ public class PlayerController : MonoBehaviour
 
     //Movement Input
     private string _inputHorizontal = "Horizontal";
-    private string _inputVertical= "Vertical";
-    private string _inputInteraction = "E";
+    private string _inputVertical = "Vertical";
+    // private string _inputInteraction = "E";
 
-    //private void Awake ()
-    //{
-    //    _animatorController = GetComponent<AnimatorController> ();
-    //}
-
-    private void Update ()
+    private void Awake()
     {
-        Movement ();
-        Interaction ();
+        _animatorController = GetComponent<AnimatorController>();
     }
 
-    private void Movement ()
+    private void Update()
     {
-        _moveHorizontal = Input.GetAxisRaw (_inputHorizontal);
-        _moveVertical = Input.GetAxisRaw (_inputVertical);
+        Movement();
+        Interaction();
+    }
+
+    private void Movement()
+    {
+        _moveHorizontal = Input.GetAxisRaw(_inputHorizontal);
+        _moveVertical = Input.GetAxisRaw(_inputVertical);
 
         _posX = _moveHorizontal * speed * Time.deltaTime;
         _posZ = _moveVertical * speed * Time.deltaTime;
 
-        transform.Translate (_posX, 0, _posZ);
+        transform.Translate(_posX, 0, _posZ);
 
-        //_animatorController.Movement (_posX, _posZ);
+        _animatorController.Movement(_moveHorizontal, _moveVertical);
     }
 
-    private void Interaction ()
+    private void Interaction()
     {
-        if (Input.GetKey (_inputInteraction))
-        {
-            // sphere gizmo interact w quad to open chest w colliders
-            // Debug.Log ($"<b> Chest Open </b>");
-        }
+        // if (Input.GetKey (_inputInteraction))
+        // {
+        //    // sphere gizmo interact w quad to open chest w colliders
+        //    // Debug.Log ($"<b> Chest Open </b>");
+        // }
     }
 }
--- WYHBM/Assets/Scripts/Utility/Animations/AnimatorController.cs
+++ WYHBM/Assets/Scripts/Utility/Animations/AnimatorController.cs
@@ -1,31 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer))]
 public class AnimatorController : MonoBehaviour
 {
     private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
 
-    private AnimationCommandBool _animIsAlive = new AnimIsAlive ();
-    private AnimationCommandBool _animModeCombat= new AnimModeCombat();
+    private bool _isFlipped;
+
+    private AnimationCommandBool _animIsAlive = new AnimIsAlive();
+    private AnimationCommandBool _animModeCombat = new AnimModeCombat();
     private AnimationCommandFloat _animValueX = new AnimValueX();
-    private AnimationCommandFloat _animValueY = new AnimValueY ();
-    private AnimationCommandBool _animActionAttack = new AnimActionAttack ();
-    private AnimationCommandBool _animActionItem = new AnimActionItem ();
-    private AnimationCommandBool _animActionReceiveDamage = new AnimActionReceiveDamage ();
-    private AnimationCommandInt _animActionType = new AnimActionType ();
+    private AnimationCommandFloat _animValueY = new AnimValueY();
+    private AnimationCommandBool _animActionAttack = new AnimActionAttack();
+    private AnimationCommandBool _animActionItem = new AnimActionItem();
+    private AnimationCommandBool _animActionReceiveDamage = new AnimActionReceiveDamage();
+    private AnimationCommandInt _animActionType = new AnimActionType();
 
-    private void Awake ()
+    private void Awake()
     {
-        _animator = GetComponent<Animator> ();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public void Movement (float valueX, float valueY)
+    public void Movement(float valueX, float valueY)
     {
-        _animValueX.Execute (_animator, valueX);
-        _animValueY.Execute (_animator, valueY);
+        FlipSprite(valueX);
+        
+        _animValueX.Execute(_animator, valueX);
+        _animValueY.Execute(_animator, valueY);
     }
 
-    
+    private void FlipSprite(float valueX)
+    {
+        if (valueX < 0)
+        {
+            _isFlipped = true;
+        }
+        else if (valueX > 0)
+        {
+            _isFlipped = false;
+        }
+        
+        _spriteRenderer.flipX = _isFlipped;
+    }
+
 }
