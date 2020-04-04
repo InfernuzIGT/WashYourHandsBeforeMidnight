6ce33e1da46d237f6b6ce7ddf57e25ab772bf1ab
Mariano <infernuzigt@gmail.com>
2020-04-04 07:50:27 -0300

    2da Entrega - Diplomatura
    Falta combate

---
+++ WYHBM/Assets/Scripts/Utility/SelectableButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectableButton : MonoBehaviour
+{
+    public EventSystem eventSystem;
+
+    public void Select()
+    {
+        Button button = GetComponent<Button>();
+        eventSystem.SetSelectedGameObject(button.gameObject);
+    }
+}
