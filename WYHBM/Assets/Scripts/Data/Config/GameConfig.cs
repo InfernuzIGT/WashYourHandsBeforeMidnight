d85f42242ed6f86a6aa14218d7be09dd024907b1
Mariano <infernuzigt@gmail.com>
2020-04-26 22:49:55 -0300

    Other changes

--- WYHBM/Assets/Scripts/Data/Config/GameConfig.cs
+++ WYHBM/Assets/Scripts/Data/Config/GameConfig.cs
@@ -4,11 +4,13 @@
 public class GameConfig : ScriptableObject
 {
     [Header("General")]
-    public string tagPlayer = "Player";
     public Vector3 interiorPosition = new Vector3(500, 500, 500);
 
     [Header("Fade")]
     public float fadeFastDuration = 1.5f;
     public float fadeSlowDuration = 3;
+
+    [Header("Tags")]
+    [Tag] public string tagPlayer;
 
 }
