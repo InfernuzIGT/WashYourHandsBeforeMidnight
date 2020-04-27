d3914c9cf873c8769a126ec20246ee5ca1c99712
Marco <marco.graffi15@gmail.com>
2020-04-27 19:25:26 -0300

    Start Menu UI and Menu Controller

--- WYHBM/Assets/Scripts/Start Menu/MenuController.cs
+++ WYHBM/Assets/Scripts/Start Menu/MenuController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using TMPro;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
@@ -11,20 +8,34 @@ public class MenuController : MonoBehaviour
     public GameObject continueCam;
     public GameObject optionsCam;
     public GameObject extrasCam;
+    public GameObject creditsCam;
     private GameObject _lastCam;
 
     [Header ("Generals")]
-
     public GameObject mainPanel;
     public GameObject optionsPanel;
     public GameObject extrasPanel;
     public GameObject confirmPanel;
+
+    [Header ("Options")]
     public GameObject graphicsPanel;
     public GameObject audioPanel;
+    public GameObject gamePanel;
+
+    [Header ("Extras")]
+    public GameObject creditsPanel;
+
+    // GameObjects
     private GameObject _lastPanel;
-    
+
+    // Bool
     private bool _isQuitting;
     private bool _isCreatingNew;
+    private bool _isContinuing;
+    private bool _isInOptions;
+    private bool _isInExtras;
+    private bool _isInCredits;
+    private bool _isInSpecificSettings;
 
     private void Start ()
     {
@@ -32,15 +43,48 @@ public class MenuController : MonoBehaviour
         mainPanel.SetActive (true);
 
         // Last cam active set in private GO
-
     }
 
-    public void OnBackButton()
+    public void OnBackButton ()
     {
 
-        _lastCam.SetActive(true);
-        _lastPanel.SetActive(true);
-        
+        _lastCam.SetActive (true);
+        _lastPanel.SetActive (true);
+
+        if (_isInOptions)
+        {
+            optionsPanel.SetActive (false);
+
+            _isInOptions = false;
+
+        }
+
+        if (_isInSpecificSettings)
+        {
+
+            OnOptionsButton ();
+
+            _isInSpecificSettings = false;
+            _isInOptions = false;
+        }
+
+        if (_isInExtras)
+        {
+            extrasPanel.SetActive (false);
+
+            _isInExtras = false;
+
+        }
+        if (_isInCredits)
+        {
+            creditsPanel.SetActive (false);
+
+            OnExtrasButton ();
+
+            _isInCredits = false;
+            _isInExtras = true;
+        }
+
     }
 
     #region Main Menu
@@ -50,12 +94,11 @@ public class MenuController : MonoBehaviour
         _lastCam = mainCam;
         _lastPanel = mainPanel;
 
-        _lastPanel.SetActive (false);
         _lastCam.SetActive (false);
+        _lastPanel.SetActive (false);
 
-        confirmPanel.SetActive (true);
-        continueCam.SetActive (true);
-
+        SetConfirmPanel ();
+        _isContinuing = true;
     }
 
     public void OnExtrasButton ()
@@ -64,10 +107,12 @@ public class MenuController : MonoBehaviour
         _lastPanel = mainPanel;
 
         _lastCam.SetActive (false);
-        _lastPanel.SetActive(false);
+        _lastPanel.SetActive (false);
 
-        extrasCam.SetActive(true);
-        extrasPanel.SetActive(true);
+        extrasCam.SetActive (true);
+        extrasPanel.SetActive (true);
+
+        _isInExtras = true;
 
     }
 
@@ -77,10 +122,12 @@ public class MenuController : MonoBehaviour
         _lastPanel = mainPanel;
 
         optionsCam.SetActive (true);
-        optionsPanel.SetActive(true);
+        optionsPanel.SetActive (true);
 
         _lastCam.SetActive (false);
         _lastPanel.SetActive (false);
+
+        _isInOptions = true;
     }
 
     public void OnNewGameButton ()
@@ -108,29 +155,75 @@ public class MenuController : MonoBehaviour
         //Confirm text
 
     }
+    
     #endregion 
 
     #region Options Menu
 
-    public void OnGraphicsButton()
+    public void OnGameButton ()
     {
-        _lastPanel = optionsPanel;
 
-        graphicsPanel.SetActive(true);
+        _lastPanel = optionsPanel;
         
+        _lastPanel.SetActive (false);
+
+        Debug.Log ($"<b> Game settings is open </b>");
+
+        // gamePanel.SetActive (true);
+
+        _isInExtras =false;
+        _isInSpecificSettings = true;
     }
 
-    public void OnAudioButton()
+    public void OnGraphicsButton ()
+    {
+        _lastPanel = optionsPanel;
+        
+        _lastPanel.SetActive (false);
+
+        Debug.Log ($"<b> Graphics settings is open </b>");
+
+        // graphicsPanel.SetActive (true);
+
+        _isInExtras =false;
+        _isInSpecificSettings = true;
+
+    }
+
+    public void OnAudioButton ()
     {
         _lastPanel = optionsPanel;
 
-        audioPanel.SetActive(true);
-        
+        _lastPanel.SetActive (false);
+
+        Debug.Log ($"<b> Audio settings is open </b>");
+
+        // audioPanel.SetActive (true);
+
+        _isInExtras =false;
+        _isInSpecificSettings = true;
+
     }
 
     #endregion
 
     #region Extras Menu
+
+    public void OnCreditsButton ()
+    {
+        _lastCam = extrasCam;
+        _lastPanel = extrasPanel;
+
+        _lastPanel.SetActive (false);
+        _lastCam.SetActive (false);
+
+        creditsPanel.SetActive (true);
+        creditsCam.SetActive (true);
+
+        _isInCredits = true;
+        _isInExtras = false;
+
+    }
 
     #endregion
 
@@ -142,6 +235,7 @@ public class MenuController : MonoBehaviour
         confirmPanel.SetActive (true);
 
     }
+
     public void OnYesButton ()
     {
         if (_isQuitting)
@@ -151,7 +245,15 @@ public class MenuController : MonoBehaviour
         }
         if (_isCreatingNew)
         {
+            Debug.Log ($"<b> New Game is created </b>");
             // Save new data in GAMEDATA
+
+        }
+
+        if (_isContinuing)
+        {
+            Debug.Log ($"<b> Loading Game </b>");
+            // Load game data in GAMEDATA
 
         }
 
@@ -162,9 +264,9 @@ public class MenuController : MonoBehaviour
         if (_isQuitting)
         {
             mainCam.SetActive (true);
-            mainPanel.SetActive(true);
+            mainPanel.SetActive (true);
 
-            confirmPanel.SetActive(false);
+            confirmPanel.SetActive (false);
 
             _isQuitting = false;
 
@@ -172,14 +274,23 @@ public class MenuController : MonoBehaviour
         if (_isCreatingNew)
         {
             mainCam.SetActive (true);
-            mainPanel.SetActive(true);
+            mainPanel.SetActive (true);
 
-            confirmPanel.SetActive(false);
+            confirmPanel.SetActive (false);
 
             _isCreatingNew = false;
         }
 
+        if (_isContinuing)
+        {
+            mainCam.SetActive (true);
+            mainPanel.SetActive (true);
+
+            confirmPanel.SetActive (false);
+
+            _isContinuing = false;
+        }
+
     }
     #endregion
-
 }
