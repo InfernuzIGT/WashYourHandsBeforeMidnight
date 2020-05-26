6a284bcdb2f907de8e263589219da9192db6d069
Mariano <infernuzigt@gmail.com>
2020-05-25 23:53:38 -0300

    Changes in Config

--- WYHBM/Assets/Scripts/Data/Config/CombatConfig.cs
+++ WYHBM/Assets/Scripts/Data/Config/CombatConfig.cs
@@ -4,43 +4,43 @@
 public class CombatConfig : ScriptableObject
 {
     [Header("General")]
-    public float fadeDuration = 3;
+    public Vector3 positionAction = new Vector3(-.5f, 0, 0);
+    public float animationDuration = .35f;
+    [Space]
     public float startFillDuration = 5;
     public float fillDuration = .25f;
-    public float canvasFadeDuration = .35f;
-    public float fadeOutEndDuration = .75f;
     public float offsetHealthBar = 2;
 
-    [Header("Layer")]
+    [Header("Fade")]
+    public float fadeDuration = 3;
+    public float canvasFadeDuration = .35f;
+    public float fadeOutEndDuration = .75f;
+
+    [Header("Layers")]
     public LayerMask layerNone;
     public LayerMask layerPlayer;
     public LayerMask layerEnemy;
 
+    [Header("References")]
+    public CharacterUI characterUIPrefab;
+    public Actions actionsPrefab;
+    public ActionButton actionButtonPrefab;
+
     [Header("Action")]
     public float actionTimeThresholdMultiplier = 1f;
     public float waitTimeToStart = 3;
-    public float waitTimePerAction = 1.5f;
+    public float waitTimePerAction = 0.5f;
     public float waitTimeBetweenTurns = 1;
 
-    [Header("Animation - Combat")]
-    public float animationDuration = .35f;
-    public Vector3 positionAction = new Vector3(-2, 0, 0);
-    // public float positionActionX = -2;
-    // public float waitCombatDuration = 1.65f; 
-    // public float evaluationDuration = 1.25f; 
-    // public float scaleCombat = 17.5f;
-    // public float positionXCharacter = -4;
-
-    [Header("Info Text")]
-    public float infoTextMoveDuration = 1;
-    public float infoTextFadeDuration = .35f;
-    public float infoTextFadeDelay = .65f;
-    public float positionYTextStart = 1;
-    public float positionYTextEnd = 2.5f;
+    // [Header("Info Text")]
+    // public float infoTextMoveDuration = 1;
+    // public float infoTextFadeDuration = .35f;
+    // public float infoTextFadeDelay = .65f;
+    // public float positionYTextStart = 1;
+    // public float positionYTextEnd = 2.5f;
 
     [Header("Shake")]
     public float shakeDuration = .5f;
     public float shakeAmplitude = .25f;
     public float shakeFrequency = 25;
-
 }
