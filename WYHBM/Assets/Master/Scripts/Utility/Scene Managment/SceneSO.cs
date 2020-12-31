7148f87cc32b297de1c6a1d67bab2f2575742f70
Mariano <infernuzigt@gmail.com>
2020-12-30 22:40:36 -0300

    [WIP] Scene System

---
+++ WYHBM/Assets/Master/Scripts/Utility/Scene Managment/SceneSO.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+// using UnityEngine.Rendering.PostProcessing;
+
+[CreateAssetMenu(fileName = "New Scene", menuName = "Scene/Scene")]
+public class SceneSO : ScriptableObject
+{
+    [Header("Information")]
+    [TextArea] public string description;
+    [Space]
+    public SceneReference sceneMain;
+    public SceneReference[] scenesAdditive;
+
+    // TODO Mariano: Review
+
+    // [Header("Sounds")]
+    // public AudioClip music;
+    // [Range(0.0f, 1.0f)]
+    // public float musicVolume;
+
+    // [Header("Visuals")]
+    // public PostProcessProfile postprocess;
+}
---
+++ WYHBM/Assets/Scripts/Interaction/InteractionScene.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteractionScene : Interaction, IInteractable
+{
+    [Header("Cutscene")]
+    [SerializeField] private SceneSO sceneData;
+
+    private List<AsyncOperation> _listScenes;
+
+    private void Start()
+    {
+        _listScenes = new List<AsyncOperation>();
+    }
+
+    public void OnInteractionEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(Tags.Player))
+        {
+            EventController.AddListener<InteractionEvent>(OnInteractScene);
+        }
+    }
+
+    public void OnInteractionExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(Tags.Player))
+        {
+            EventController.RemoveListener<InteractionEvent>(OnInteractScene);
+        }
+    }
+
+    private void OnInteractScene(InteractionEvent evt)
+    {
+        EventController.RemoveListener<InteractionEvent>(OnInteractScene);
+        
+        // TODO Mariano: Remover Input al comenzar la carga
+        
+        LoadScenes();
+
+        // TODO Mariano: Devolver Input al cargar la escena
+    }
+
+    private void LoadScenes()
+    {
+        _listScenes.Add(SceneManager.LoadSceneAsync(sceneData.sceneMain));
+
+        if (sceneData.scenesAdditive.Length == 0)return;
+
+        for (int i = 0; i < sceneData.scenesAdditive.Length; i++)
+        {
+            _listScenes.Add(SceneManager.LoadSceneAsync(sceneData.scenesAdditive[i], LoadSceneMode.Additive));
+        }
+    }
+    
+    private IEnumerator LoadingProgress ()
+    {
+        float currentProgress = 0;
+        float totalProgress = 0;
+        
+        for (int i = 0; i < _listScenes.Count; i++)
+        {
+            while (!_listScenes[i].isDone)
+            {
+                totalProgress += _listScenes[i].progress;
+                currentProgress = totalProgress/_listScenes.Count;
+                yield return null;
+            }
+        }
+    }
+    
+}
