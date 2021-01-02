b9d2375ae9bb7188ebc257afc1dae9c1ce5f01e1
Mariano <infernuzigt@gmail.com>
2021-01-02 17:23:48 -0300

    Moved Configs to Master Folder

---
+++ WYHBM/Assets/Master/Scripts/Data/Config/PlayerConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PlayerConfig", menuName = "Config/PlayerConfig", order = 0)]
+public class PlayerConfig : ScriptableObject
+{
+    [Header("General")]
+    public float height = 3.15f;
+
+    [Header("Movement")]
+    public float speedRun = 16f;
+    public float speedJogging = 12f;
+    public float speedWalk = 5f;
+    public float speedCrouchFast = 3.5f;
+    public float speedCrouch = 2f;
+    [Space]
+    public float gravity = 39.24f;
+    public float magnitudeFall = 20f;
+
+    [Header("Input System")]
+    public float axisLimit = 0.7f;
+    public float axisLimitCrouch = 0.4f;
+
+    [Header("Layers")]
+    public LayerMask layerGround;
+    public LayerMask layerClimbable;
+
+}
