be8cdf6377e8fcb0a0bdba21ad4b23ad014e481e
Mariano <infernuzigt@gmail.com>
2021-01-26 00:48:24 -0300

    UI Design

---
+++ WYHBM/Assets/_BreakpointStudios/Scripts/UI/ButtonDoubleUI.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ButtonDoubleUI : ButtonUI
+{
+    [SerializeField, ConditionalHide] private TextMeshProUGUI _optionTxt = null;
+
+    public void SetText(string text)
+    {
+        _optionTxt.text = text;
+    }
+
+    protected override void OnSelectExtra()
+    {
+        _optionTxt.color = Color.black;
+    }
+
+    protected override void OnDeselectExtra()
+    {
+        _optionTxt.color = Color.white;
+    }
+
+}
---
+++ WYHBM/Assets/_BreakpointStudios/Scripts/UI/ButtonUI.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonUI : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    [Header("References")]
+#pragma warning disable 0414
+    [SerializeField] private bool ShowReferences = true;
+#pragma warning restore 0414
+    [SerializeField, ConditionalHide] private Button _buttonBtn = null;
+    [SerializeField, ConditionalHide] private Image _buttonImg = null;
+    [SerializeField, ConditionalHide] private TextMeshProUGUI _buttonTxt = null;
+
+    private void Start()
+    {
+        _buttonImg.enabled = false;
+        _buttonTxt.color = Color.white;
+    }
+
+    public void AddListener(UnityAction action)
+    {
+        _buttonBtn.onClick.AddListener(action);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        _buttonImg.enabled = true;
+        _buttonTxt.color = Color.black;
+        OnSelectExtra();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        _buttonImg.enabled = false;
+        _buttonTxt.color = Color.white;
+        OnDeselectExtra();
+    }
+
+    protected virtual void OnSelectExtra() { }
+    protected virtual void OnDeselectExtra() { }
+
+}
