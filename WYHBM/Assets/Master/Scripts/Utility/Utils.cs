dd5019f7066900547f257a0cc1cc4e503180dba5
Mariano <infernuzigt@gmail.com>
2021-01-28 01:13:04 -0300

    Added Utils script

---
+++ WYHBM/Assets/Master/Scripts/Utility/Utils.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public static class Utils
+{
+    public static int GetNextIndex(bool isNext, int index, int max, bool reset = true)
+    {
+        if (reset)
+        {
+            if (isNext)
+            {
+                index = index < max ? index + 1 : 0;
+            }
+            else
+            {
+                index = index > 0 ? index - 1 : max;
+            }
+        }
+        else
+        {
+            if (isNext)
+            {
+                index = index < max ? index + 1 : index;
+            }
+            else
+            {
+                index = index > 0 ? index - 1 : 0;
+            }
+        }
+
+        return index;
+    }
+}
