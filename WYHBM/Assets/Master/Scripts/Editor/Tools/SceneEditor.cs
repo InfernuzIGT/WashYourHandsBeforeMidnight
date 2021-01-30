89358654f343cabd99b91c3d7ba0a52fee51c20b
Mariano <infernuzigt@gmail.com>
2021-01-30 11:48:19 -0300

    Improvements in SceneEditor Tool

--- WYHBM/Assets/Master/Scripts/Editor/Tools/SceneEditor.cs
+++ WYHBM/Assets/Master/Scripts/Editor/Tools/SceneEditor.cs
@@ -8,7 +8,12 @@ public class SceneEditor : EditorWindow
     // Scene Editor
     public SceneSO sceneData;
 
+    private string _pathMainMenu = "Assets/Master/Main Menu.unity";
+    private string _pathPersistent = "Assets/Master/Persistent.unity";
+    private string _pathPlayer = "Assets/Master/Player.unity";
+
     private GUIStyle _styleButtons;
+    private GUIStyle _styleButtonBig;
 
     [MenuItem("Tools/Scene Editor")]
     static void CreateColliderWindow()
@@ -17,26 +22,75 @@ public class SceneEditor : EditorWindow
         Texture2D iconTitle = EditorGUIUtility.Load(EditorGUIUtility.isProSkin ? "SceneAsset On Icon" : "SceneAsset Icon")as Texture2D;
         GUIContent titleContent = new GUIContent("Scene Editor", iconTitle);
         window.titleContent = titleContent;
-        window.minSize = new Vector2(325, 120);
+        window.minSize = new Vector2(325, 395);
         window.Show();
     }
 
     private void OnGUI()
     {
         _styleButtons = new GUIStyle(GUI.skin.button) { alignment = TextAnchor.MiddleCenter, fixedHeight = 30 };
+        _styleButtonBig = new GUIStyle(GUI.skin.button) { alignment = TextAnchor.MiddleCenter, fixedHeight = 60 };
 
         EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        Scene("Main Menu", _pathMainMenu);
+        EditorGUILayout.Space();
+        Scene("Persistent", _pathPersistent);
+        EditorGUILayout.Space();
+        Scene("Player", _pathPlayer);
+        EditorGUILayout.Space();
+
+        SceneData();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        DrawHorizontalLine();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Save All", _styleButtonBig))
+        {
+            // EditorSceneManager.MarkAllScenesDirty();
+            EditorSceneManager.SaveOpenScenes();
+        }
+
+        // DrawSize();
+    }
+
+    private void Scene(string name, string path)
+    {
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Scene: " + name);
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Load", _styleButtons))
+        {
+            EditorSceneManager.OpenScene(path, OpenSceneMode.Additive);
+        }
+
+        if (GUILayout.Button("Unload", _styleButtons))
+        {
+            EditorSceneManager.CloseScene(SceneManager.GetSceneByPath(path), true);
+        }
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+    }
+
+    private void SceneData()
+    {
         EditorGUILayout.Space();
 
         sceneData = (SceneSO)EditorGUILayout.ObjectField("Scene Data", sceneData, typeof(SceneSO), true);
-
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
 
         EditorGUILayout.BeginHorizontal();
 
         if (GUILayout.Button("Load", _styleButtons))
         {
+            if (sceneData == null)return;
+
             for (int i = 0; i < sceneData.scenes.Length; i++)
             {
                 EditorSceneManager.OpenScene(sceneData.scenes[i], OpenSceneMode.Additive);
@@ -45,6 +99,8 @@ public class SceneEditor : EditorWindow
 
         if (GUILayout.Button("Unload", _styleButtons))
         {
+            if (sceneData == null)return;
+
             for (int i = 0; i < sceneData.scenes.Length; i++)
             {
                 EditorSceneManager.CloseScene(SceneManager.GetSceneByPath(sceneData.scenes[i].ScenePath), true);
@@ -52,14 +108,7 @@ public class SceneEditor : EditorWindow
         }
 
         EditorGUILayout.EndHorizontal();
-
-        if (GUILayout.Button("Save All", _styleButtons))
-        {
-            // EditorSceneManager.MarkAllScenesDirty();
-            EditorSceneManager.SaveOpenScenes();
-        }
-
-        // DrawSize();
+        EditorGUILayout.Space();
     }
 
     private void DrawHorizontalLine()
